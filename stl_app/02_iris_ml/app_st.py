57ae69eafb0eb46873061e4a59ab81c5b3bcffaf
66056073 <66056073@kmitl.ac.th>
2023-11-16 02:15:06 +0700

    Clone code from 04_penguin_ml and edit to use with iris dataset

--- stl_app/02_iris_ml/app_st.py
+++ stl_app/02_iris_ml/app_st.py
@@ -14,3 +14,35 @@ st.write('This app uses 4 inputs to predict the species of iris using'
 
 iris_file = st.file_uploader('Upload your own iris dataset')
 
+if iris_file is None:
+    rf_pickle = open('random_forest_iris.pickle', 'rb')
+    map_pickle = open('output_iris.pickle', 'rb')
+
+    rfc = pickle.load(rf_pickle)
+    unique_iris_mapping = pickle.load(map_pickle)
+
+    rf_pickle.close()
+else:
+    iris_df = pd.read_csv(iris_file)
+    iris_df = iris_df.dropna()
+
+    output = iris_df['variety']
+    features = iris_df[['sepal.length', 'sepal.width', 'petal.length', 'petal.width']]
+
+    features = pd.get_dummies(features)
+
+    output, unique_iris_mapping = pd.factorize(output)
+
+    x_train, x_test, y_train, y_test = train_test_split(
+        features, output, test_size=0.8)
+
+    rfc = RandomForestClassifier(random_state=15)
+    rfc.fit(x_train, y_train)
+
+    y_pred = rfc.predict(x_test)
+
+    score = round(accuracy_score(y_pred, y_test), 2)
+
+    st.write('We trained a Random Forest model on these data,'
+             ' it has a score of {}! Use the '
+             'inputs below to try out the model.'.format(score))
