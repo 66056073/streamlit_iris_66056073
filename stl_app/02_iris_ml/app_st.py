488a1ec4156f439211adf4cf2cc9f7058da6ee0b
66056073 <66056073@kmitl.ac.th>
2023-11-16 02:06:45 +0700

    Import library

---
+++ stl_app/02_iris_ml/app_st.py
@@ -0,0 +1,8 @@
+import streamlit as st
+import seaborn as sns
+import matplotlib.pyplot as plt
+import pandas as pd
+import pickle
+from sklearn.metrics import accuracy_score
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import train_test_split
