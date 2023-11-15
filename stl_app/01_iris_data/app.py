4be060318c5599928c6c922d4e4e1d96d0506cfc
66056073 <66056073@kmitl.ac.th>
2023-11-16 00:31:31 +0700

    Add title and choices

--- stl_app/01_iris_data/app.py
+++ stl_app/01_iris_data/app.py
@@ -2,3 +2,11 @@ import streamlit as st
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
+
+st.title('Iris Species')
+st.markdown('`Scatter plot` show **Iris Species**')
+
+choices = ['sepal.length',
+           'sepal.width',
+           'petal.length',
+           'petal.width']
