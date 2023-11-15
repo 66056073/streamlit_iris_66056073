42702a6bc760e8127e7df31beb17d3d67076a6ea
66056073 <66056073@kmitl.ac.th>
2023-11-16 01:36:48 +0700

    Change Import library

--- stl_app/02_iris_ml/app_ml.py
+++ stl_app/02_iris_ml/app_ml.py
@@ -1,9 +1,11 @@
-import streamlit as st
-import seaborn as sns
-import matplotlib.pyplot as plt
 import pandas as pd
-import pickle
+import matplotlib.pyplot as plt
+import seaborn as sns
 from sklearn.metrics import accuracy_score
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import train_test_split
 
+import pickle
+
+
+st.
