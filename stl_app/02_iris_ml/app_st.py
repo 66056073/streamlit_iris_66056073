904a3039591492601be8bf33a59cac71f296b6ce
66056073 <66056073@kmitl.ac.th>
2023-11-16 02:08:29 +0700

    Commit Title

--- stl_app/02_iris_ml/app_st.py
+++ stl_app/02_iris_ml/app_st.py
@@ -6,3 +6,8 @@ import pickle
 from sklearn.metrics import accuracy_score
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import train_test_split
+
+st.title('Iris Classifier')
+st.write('This app uses 4 inputs to predict the species of iris using'
+         'a model built on the Iris dataset. Use the form below'
+         ' to get started!')
